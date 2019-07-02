e527f97709d14dd6ea8cc14c40dc71f0c32a65a7
dejwsz <dejwsz@gmail.com>
2019-07-02 14:22:32 +0200

    display header

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "X-Forwarded-For header value: " + request.environ['HTTP_X_FORWARDED_FOR']
 
 if __name__ == "__main__":
     application.run()
