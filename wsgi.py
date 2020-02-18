0fd5f986c3992ab9589aaf174d03c4f122e6c5b6
dejwsz <dejwsz@gmail.com>
2020-02-18 20:33:44 +0100

    Update wsgi.py
    add all headers

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "X-Forwarded-For header value: " + request.environ['HTTP_X_FORWARDED_FOR']
+    return "X-Forwarded-For header value: " + request.environ['HTTP_X_FORWARDED_FOR'] + "\n" +
+           "all headers: " + request.headers 
 
 if __name__ == "__main__":
     application.run()
